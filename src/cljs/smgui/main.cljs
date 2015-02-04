6b99c6237e91fa9a911a289aadc094abcd5444a5
Wilker Lucio <wilkerlucio@gmail.com>
2015-02-04 07:02:48 -0300

    disable updater

--- src/cljs/smgui/main.cljs
+++ src/cljs/smgui/main.cljs
@@ -61,7 +61,7 @@
 (defn init []
   (ws-repl/connect "ws://localhost:9001")
   (enable-console-print!)
-  (updater-main)
+  #_ (updater-main)
   (gui/listen-open #(do (put! flux-channel {:cmd :add-search :path %})
                         (put! flux-channel {:cmd :change-page :page :search})))
   (om/root main-view app-state {:target (.-body js/document)})
